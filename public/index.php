ca3da6c693b4b6938a7c03d0e8364d27354b63bd
Eli V <elivol333@gmail.com>
2026-03-23 22:38:47 +0200

    fix: docker + nginx + debug for render deployment config

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-die('Laravel is running');
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
