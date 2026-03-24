ec3edc6f4602752993021b1246a33017cc50e877
Eli V <elivol333@gmail.com>
2026-03-24 21:16:28 +0200

    fix: vite css path mismatch

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
+      input: ['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'],
       refresh: true, // HMR
       server: {
         hmr: {
