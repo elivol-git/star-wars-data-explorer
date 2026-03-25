bd21c71368c211e760a3c701bd32b0c1c40a8abf
Eli V <elivol333@gmail.com>
2026-03-25 20:46:51 +0200

    fix: force https for assets, load URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Services\Llm\Clients\HuggingFaceClient;
 use App\Services\Llm\Clients\LlmClientInterface;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
