452a864d0e21d5015f3a8367777c4635548dda63
Eli V <elivol333@gmail.com>
2026-04-09 18:00:49 +0300

    Set https on dev

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,28 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'],
+      input: [
+        'resources/css/app.css',
+        'resources/css/style.css',
+        'resources/js/app.js'
+      ],
       refresh: true,
-      // Dev server HMR (kept for local dev)
-      server: {
-        hmr: {
-          host: 'localhost',
-        },
-      },
     }),
     vue(),
   ],
-  base: '/',
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+
+    https: {
+      key: './docker/ssl/film-planets.test-key.pem',
+      cert: './docker/ssl/film-planets.test.pem',
+    },
+
+    hmr: {
+      host: 'film-planets.test',
+      protocol: 'wss',
+    },
+  },
 });
